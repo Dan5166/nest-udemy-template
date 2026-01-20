869a13d5161ffe116469cb831f87f65401af5c6f
Danyael Natan Vasquez Ojeda <dnvasquez@iplacex.cl>
2026-01-20 15:59:30 -0300

    ADD clase 167

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt'
+import { userInfo } from 'os';
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -15,12 +18,45 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
-      await this.userRepository.save(user);
-      return user;
+
+
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      });
+
+
+      await this.userRepository.save(user)
+
+      const {password: pass, ...userInfo} = user;
+
+      return userInfo;
+
+      // TODO: Retornar el JWT de acceso
+
     } catch (error) {
       this.handleDBErrors(error)
     }
+  }
+
+  async login ( loginUserDto: LoginUserDto ) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select: {email: true, password: true}
+    });
+
+    if (!user) throw new UnauthorizedException('Not valid credentials ema')
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials pass')
+    }
+
+    return user;
+    // TODO: Retornar JWT
   }
 
   private handleDBErrors(error: any): never {
